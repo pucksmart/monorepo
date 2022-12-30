5a373bcb02f0ad6096d3ceb64ff68129fc21c49a
Abbi Devins-Suresh <aesther2012@gmail.com>
2022-12-30 16:05:08 -0500

    adding in notes

--- backend/src/main/java/dev/pucksmart/Season.java
+++ backend/src/main/java/dev/pucksmart/Season.java
@@ -1,4 +1,17 @@
 package dev.pucksmart;
 
+import java.time.LocalDate;
+
 public class Season {
+// use these attributes
+//    String seasonId;
+//    LocalDate regularSeasonStartDate;
+//    LocalDate regularSeasonEndDate;
+//    LocalDate seasonEndDate;
+//    Integer numberOfGames;
+//    Boolean tiesInUse;
+//    Boolean olympicsParticipation;
+//    Boolean conferencesInUse;
+//    Boolean divisionsInUse;
+//    Boolean wildCardInUse;
 }
